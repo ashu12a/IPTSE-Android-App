0de017060def676ab4fa9bfbb1111f7312ccc35c
ashu12a <ashutoshsharma86416@gmail.com>
2024-05-16 14:36:22 +0530

    IPTSE Android App

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { BackHandler, StatusBar, Dimensions, StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const webViewRef = useRef(null);
+
+  const handleBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  };
+
+  React.useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    return () => BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <WebView
+        ref={webViewRef}
+        style={styles.web}
+        source={{ uri: 'https://iptse.com/exam/' }}
+      />
     </View>
   );
 }
+
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +38,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  web: {
+    width: width,
+    height: height,
+    marginTop: 0,
   },
 });
